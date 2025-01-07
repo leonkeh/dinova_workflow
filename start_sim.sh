f92a5de1eb60dee156832bff0bd8b3b3f04ff8ec
leon <Leon.Kehler@outlook.com>
2025-01-07 13:36:21 +0100

    accomendated switching and example sims

--- start_sim.sh
+++ start_sim.sh
@@ -1,16 +1,31 @@
 #!/bin/bash
 
-ros_sourcing_script='source ./workflow/source_ros.sh'
+ros_sourcing_script='source ./workflow/source_ros.sh; export ROBOT_NAME=dinova'
 
 # launch the ros master
 gnome-terminal --tab --title="Simulation Master" -- bash -c "\
 $ros_sourcing_script; roslaunch dinova_gazebo dinova.launch mode:=velocity; exec bash"
 
-# Second tab: Navigate to the user's home directory and list files
-gnome-terminal --tab --title="Action Server" -- bash -c "\
-$ros_sourcing_script; sleep 3; \
-roslaunch dinova_fabrics_wrapper dinova_joint_space_gazebo.launch lidar:=false with_obstacles:=true; exec bash"
+if [ -z "$1" ]; then
+    echo "The example simulation is running"
+    # launch the action server
+    gnome-terminal --tab --title="Action Server" -- bash -c "\
+    $ros_sourcing_script; sleep 3; \
+    roslaunch dinova_fabrics_wrapper dinova_joint_space_gazebo.launch lidar:=false with_obstacles:=true; exec bash"
 
-# # Third tab: Ping a website to check connectivity
-gnome-terminal --tab --title="Action Client" -- bash -c "\
-$ros_sourcing_script; rosrun dinova_fabrics_wrapper dinova_joint_client.py dinova; exec bash"
+    # call the action client
+    gnome-terminal --tab --title="Action Client" -- bash -c "\
+    $ros_sourcing_script; rosrun dinova_fabrics_wrapper dinova_joint_client.py dinova; exec bash"
+elif [ "$1" == "switching" ]; then
+    echo "The switching simulation is running"
+    # launch the action server
+    gnome-terminal --tab --title="Action Server" -- bash -c "\
+    $ros_sourcing_script; sleep 3; \
+    roslaunch switched_control switched_fabrics.launch lidar:=false with_obstacles:=true; exec bash"
+
+    # call the action client
+    gnome-terminal --tab --title="Action Client" -- bash -c "\
+    $ros_sourcing_script; rosrun switched_control switched_client.py dinova; exec bash"
+else
+    echo "$1 is not a known simulation type"
+fi
